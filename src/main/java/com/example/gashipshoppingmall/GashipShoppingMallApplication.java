627d46a7771507130b85d449b2d9f04b05f05435
HoFe-U <hochul-y@naver.com>
2022-07-07 09:43:39 +0900

    first 👑 : gaship-shopping-mall 프로젝트 생성

---
+++ src/main/java/com/example/gashipshoppingmall/GashipShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.gashipshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GashipShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GashipShoppingMallApplication.class, args);
+	}
+
+}
