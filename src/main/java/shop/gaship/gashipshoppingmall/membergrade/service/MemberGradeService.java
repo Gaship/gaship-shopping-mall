1890d99037db1e84ff8100f990e16727a8b792f2
semi <semikimm.m@gmail.com>
2022-07-10 23:05:35 +0900

    [feat✨]:회원등급 등록/수정/삭제 추가

---
+++ src/main/java/shop/gaship/gashipshoppingmall/membergrade/service/MemberGradeService.java
@@ -0,0 +1,47 @@
+package shop.gaship.gashipshoppingmall.membergrade.service;
+
+import shop.gaship.gashipshoppingmall.membergrade.request.MemberGradeRequest;
+
+/**
+ * packageName    : shop.gaship.gashipshoppingmall.membergrade.service
+ * fileName       : MemberGradeService
+ * author         : Semi Kim
+ * date           : 2022/07/09
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/07/09        Semi Kim       최초 생성
+ */
+public interface MemberGradeService {
+    /**
+     * .
+     * methodName : addMemberGrade
+     * author : Semi Kim
+     * description :
+     *
+     * @param request MemberGradeRequest
+     */
+    void addMemberGrade(MemberGradeRequest request);
+
+    /**
+     * .
+     * methodName : modifyMemberGrade
+     * author : Semi Kim
+     * description :
+     *
+     * @param memberGradeNo Integer
+     * @param request       MemberGradeRequest
+     */
+    void modifyMemberGrade(Integer memberGradeNo, MemberGradeRequest request);
+
+    /**
+     * .
+     * methodName : removeMemberGrade
+     * author : Semi Kim
+     * description :
+     *
+     * @param memberGradeNo Integer
+     */
+    void removeMemberGrade(Integer memberGradeNo);
+}
