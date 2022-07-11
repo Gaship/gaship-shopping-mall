613d22e4de6482c0bf7abf9ab382ae260e10ed4b
semi <semikimm.m@gmail.com>
2022-07-11 10:21:07 +0900

    [feat✨] : 상태코드 Entity 클래스 작성, 상태코드 레포지토리 생성

---
+++ src/main/java/shop/gaship/gashipshoppingmall/statuscode/dto/StatusCodeDto.java
@@ -0,0 +1,16 @@
+package shop.gaship.gashipshoppingmall.statuscode.dto;
+
+/**
+ * packageName    : shop.gaship.gashipshoppingmall.statuscode.dto
+ * fileName       : StatusCodeDto
+ * author         : Semi Kim
+ * date           : 2022/07/10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/07/10        Semi Kim       최초 생성
+ */
+public interface StatusCodeDto {
+    String getStatusCodeName();
+}
---
+++ src/main/java/shop/gaship/gashipshoppingmall/statuscode/entity/StatusCode.java
@@ -0,0 +1,53 @@
+package shop.gaship.gashipshoppingmall.statuscode.entity;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * packageName    : shop.gaship.gashipshoppingmall.statuscode.entity
+ * fileName       : StatusCode
+ * author         : Semi Kim
+ * date           : 2022/07/09
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/07/09        Semi Kim       최초 생성
+ */
+@Entity
+@Table(name = "status_codes")
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class StatusCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "status_code_no")
+    private Integer statusCodeNo;
+
+    @Column(name = "status_code_name")
+    private String statusCodeName;
+
+    @Column(name = "is_used")
+    private Boolean isUsed;
+
+    private Integer priority;
+
+    @Column(name = "group_code_name")
+    private String groupCodeName;
+
+    private String explanation;
+
+    @Builder
+    public StatusCode(String statusCodeName, Integer priority, String groupCodeName, String explanation) {
+        this.statusCodeName = statusCodeName;
+        this.isUsed = true;
+        this.priority = priority;
+        this.groupCodeName = groupCodeName;
+        this.explanation = explanation;
+    }
+}
