86457e4708827c14c426708c51218682c8136078
semi <semikimm.m@gmail.com>
2022-07-12 15:22:26 +0900

    [feat✨] : Member 레포지토리 메서드 추가 (findByMemberGrades(MemberGrade memberGrade))

--- src/main/java/shop/gaship/gashipshoppingmall/member/repository/MemberRepository.java
+++ src/main/java/shop/gaship/gashipshoppingmall/member/repository/MemberRepository.java
@@ -2,9 +2,13 @@ package shop.gaship.gashipshoppingmall.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.gaship.gashipshoppingmall.member.entity.Member;
+import shop.gaship.gashipshoppingmall.membergrade.entity.MemberGrade;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member,Integer> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findByNickname(String recommendMemberNickname);
+
+    List<Member> findByMemberGrades(MemberGrade memberGrade);
 }
