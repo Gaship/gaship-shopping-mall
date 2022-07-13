76eb0bf1e03f7299a0b623f1c58c407e5ce316f7
oct-sky-out <kms3335k@naver.com>
2022-07-14 02:49:29 +0900

    [refactor🔨] : 회원생성 validation 정규표현식 적용

--- src/main/java/shop/gaship/gashipshoppingmall/member/dto/MemberCreationRequest.java
+++ src/main/java/shop/gaship/gashipshoppingmall/member/dto/MemberCreationRequest.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -32,6 +33,11 @@ public class MemberCreationRequest {
 
     @NotBlank
     @Length(max = 100)
+    @Pattern(
+        // 첫글자는 대문자, 소문자, 숫자, @$!%*#?& 특수문자만 허용하여 8자리 이상 조합
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+        message = "영문, 숫자, 특수문자를 포함하여 8자리 이상이어야합니다."
+    )
     private String password;
 
     @NotBlank
@@ -40,6 +46,10 @@ public class MemberCreationRequest {
 
     @NotBlank
     @Length(max = 100)
+    @Pattern(
+        regexp = "^[가-힣]+",
+        message = "이름을 정확히 입력하여주십시오."
+    )
     private String name;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-dd")
@@ -47,6 +57,10 @@ public class MemberCreationRequest {
 
     @NotBlank
     @Length(max = 20)
+    @Pattern(
+        regexp = "^[가-힣a-zA-Z\\d]{0,20}",
+        message = "닉네임은 한글 또는 영문, 숫자만 입력이 가능합니다."
+    )
     private String nickName;
 
     @NotBlank
