e9fd552742f5430f9210b3cd882ba7e7a625f194
choijungwoo493 <jwoo1016@gmail.com>
2022-07-13 14:15:28 +0900

    [feat✨] : memberServiceImplTest 스켈레톤 생성

---
+++ src/test/java/shop/gaship/gashipshoppingmall/member/service/MemberServiceImplTest.java
@@ -0,0 +1,39 @@
+package shop.gaship.gashipshoppingmall.member.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName    : shop.gaship.gashipshoppingmall.member.service
+ * fileName       : MemberServiceImplTest
+ * author         : choijungwoo
+ * date           : 2022/07/13
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/07/13        choijungwoo       최초 생성
+ */
+class MemberServiceImplTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getList() {
+    }
+}
