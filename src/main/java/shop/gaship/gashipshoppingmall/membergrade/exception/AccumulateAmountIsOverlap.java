9f067f08d5bd968dc3882cb2064bccbe92c8d544
semi <semikimm.m@gmail.com>
2022-07-14 16:56:45 +0900

    [feat✨] : 회원등급 관련 중복누적금액 및 중복기본등급 처리 exception 추가

---
+++ src/main/java/shop/gaship/gashipshoppingmall/membergrade/exception/AccumulateAmountIsOverlap.java
@@ -0,0 +1,18 @@
+package shop.gaship.gashipshoppingmall.membergrade.exception;
+
+/**
+ * packageName    : shop.gaship.gashipshoppingmall.membergrade.exception
+ * fileName       : AccumulateAmountIsOverlap
+ * author         : Semi Kim
+ * date           : 2022/07/14
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/07/14        Semi Kim       최초 생성
+ */
+public class AccumulateAmountIsOverlap extends RuntimeException {
+    public AccumulateAmountIsOverlap(String message) {
+        super(message);
+    }
+}
