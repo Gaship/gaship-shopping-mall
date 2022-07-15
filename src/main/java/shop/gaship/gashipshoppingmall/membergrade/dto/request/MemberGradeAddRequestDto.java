0c9a878fa52ce1749b208cedf0362644788effd3
semi <semikimm.m@gmail.com>
2022-07-15 18:13:12 +0900

    [fix🚑] : 문자열 길이 관련 validation annotation 수정 (@Size() -> @Length())

--- src/main/java/shop/gaship/gashipshoppingmall/membergrade/dto/request/MemberGradeAddRequestDto.java
+++ src/main/java/shop/gaship/gashipshoppingmall/membergrade/dto/request/MemberGradeAddRequestDto.java
@@ -2,9 +2,9 @@ package shop.gaship.gashipshoppingmall.membergrade.dto.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * packageName    : shop.gaship.gashipshoppingmall.membergrade.request
@@ -21,7 +21,7 @@ import lombok.Setter;
 @Setter
 public class MemberGradeAddRequestDto {
     @NotBlank(message = "name 은 필수 입력값입니다.")
-    @Size(min = 1, max = 10)
+    @Length(min = 1, max = 10, message = "name 의 길이는 최소 1 최대 10 입니다.")
     private String name;
     @NotNull(message = "accumulateAmount 은 필수 입력값입니다.")
     private Long accumulateAmount;
--- src/main/java/shop/gaship/gashipshoppingmall/membergrade/dto/request/MemberGradeModifyRequestDto.java
+++ src/main/java/shop/gaship/gashipshoppingmall/membergrade/dto/request/MemberGradeModifyRequestDto.java
@@ -2,9 +2,9 @@ package shop.gaship.gashipshoppingmall.membergrade.dto.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * packageName    : shop.gaship.gashipshoppingmall.membergrade.dto.request
@@ -22,7 +22,7 @@ import lombok.Setter;
 public class MemberGradeModifyRequestDto {
     @NotNull
     private Integer no;
-    @Size(min = 1, max = 10)
+    @Length(min = 1, max = 10)
     @NotBlank(message = "name 은 필수 입력값입니다.")
     private String name;
     @NotNull(message = "accumulateAmount 는 필수 입력값입니다.")
