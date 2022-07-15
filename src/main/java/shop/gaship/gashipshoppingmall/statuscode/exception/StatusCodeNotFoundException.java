a5a61cbb9a0c1a77d7b310ddc29af406dfcc3604
semi <semikimm.m@gmail.com>
2022-07-15 17:45:14 +0900

    [refactor🔨] : status code 관련 exception message 피드백 반영

--- src/main/java/shop/gaship/gashipshoppingmall/statuscode/exception/StatusCodeNotFoundException.java
+++ src/main/java/shop/gaship/gashipshoppingmall/statuscode/exception/StatusCodeNotFoundException.java
@@ -12,4 +12,9 @@ package shop.gaship.gashipshoppingmall.statuscode.exception;
  * 2022/07/09        Semi Kim       최초 생성
  */
 public class StatusCodeNotFoundException extends RuntimeException {
+    private static final String MESSAGE = "해당 상태 코드를 찾을 수 없습니다.";
+
+    public StatusCodeNotFoundException() {
+        super(MESSAGE);
+    }
 }
