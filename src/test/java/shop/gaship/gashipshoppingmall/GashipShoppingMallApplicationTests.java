f75ae793c7c12125b6e16ef5c74aed2046a1be75
HoFe-U <hochul-y@naver.com>
2022-07-18 10:24:01 +0900

    Revert "[delete🗑] : 안쓰는 테스트코드삭제"
    This reverts commit be8601d1ed155eb708b8937a3373c3cbed584841.

---
+++ src/test/java/shop/gaship/gashipshoppingmall/GashipShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.gaship.gashipshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GashipShoppingMallApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
