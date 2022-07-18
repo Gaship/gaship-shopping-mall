bb0bea7cd06ed2277372a928342ffed8a0368087
semi <semikimm.m@gmail.com>
2022-07-18 13:03:30 +0900

    [docs📖] : 상태코드 JavaDoc 추가

--- src/main/java/shop/gaship/gashipshoppingmall/statuscode/repository/StatusCodeRepository.java
+++ src/main/java/shop/gaship/gashipshoppingmall/statuscode/repository/StatusCodeRepository.java
@@ -12,7 +12,21 @@ import shop.gaship.gashipshoppingmall.statuscode.entity.StatusCode;
  */
 public interface StatusCodeRepository
         extends JpaRepository<StatusCode, Integer>, StatusCodeRepositoryCustom {
+    /**
+     * 그룹코드 명을 통해 갱신기간 단건 조회.
+     *
+     * @param group 그룹코드 명으로 갱신기간을 조회하기 위한 상태 그룹 코드 변수 (String).
+     * @return optional StatusCode 를 담는 Optional 반환.
+     * @author 김세미
+     */
     Optional<StatusCode> findByGroupCodeName(String group);
 
+    /**
+     * 상태코드 명을 통해 상태코드 단건 조회.
+     *
+     * @param statusCodeName 상태코드명으로 상태코드 단건 조회를 하기 위한 상태 코드 변수 (String).
+     * @return optional StatusCode 를 담는 Optional 반환.
+     * @author 김세미
+     */
     Optional<StatusCode> findByStatusCodeName(String statusCodeName);
 }
--- src/main/java/shop/gaship/gashipshoppingmall/statuscode/service/StatusCodeService.java
+++ src/main/java/shop/gaship/gashipshoppingmall/statuscode/service/StatusCodeService.java
@@ -10,7 +10,20 @@ import shop.gaship.gashipshoppingmall.statuscode.dto.response.StatusCodeResponse
  * @since 1.0
  */
 public interface StatusCodeService {
+    /**
+     * 갱신기간 상태코드 값 수정.
+     *
+     * @param period String
+     * @author 김세미
+     */
     void modifyRenewalPeriod(String period);
 
+    /**
+     * 상태코드 그룹코드를 통해 해당 그룹 코드에 속해있는 상태코드 목록 조회.
+     *
+     * @param groupCodeName String
+     * @return list StatusCodeResponseDto 를 담고 있는 List 반환.
+     * @author 김세미
+     */
     List<StatusCodeResponseDto> findStatusCodes(String groupCodeName);
 }
