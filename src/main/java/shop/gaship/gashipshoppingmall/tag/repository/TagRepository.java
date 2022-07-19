503cb338a603149d44edd22b05c59cbf94a80d86
choijungwoo493 <jwoo1016@gmail.com>
2022-07-19 11:53:30 +0900

    [feat✨] : TagRepository 에 id 리스트를 넘기면 엔티티 리스트를 가져오는 메소드 추가

--- src/main/java/shop/gaship/gashipshoppingmall/tag/repository/TagRepository.java
+++ src/main/java/shop/gaship/gashipshoppingmall/tag/repository/TagRepository.java
@@ -4,6 +4,8 @@ package shop.gaship.gashipshoppingmall.tag.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.gaship.gashipshoppingmall.tag.entity.Tag;
 
+import java.util.List;
+
 /**
  * tag에 관련된 db 정보를 관리하는 repository입니다.
  *
@@ -18,4 +20,12 @@ public interface TagRepository extends JpaRepository<Tag,Integer> {
      * @return the boolean
      */
     boolean existsByTitle(String title);
+
+    /**
+     * 회원이 선택한 태그들을 리스트형태로 가져올때 쓰이는 메서드
+     *
+     * @param tagIds the title
+     * @return the List<Tag>
+     */
+    List<Tag> findByIdIn(List<Integer> tagIds);
 }
