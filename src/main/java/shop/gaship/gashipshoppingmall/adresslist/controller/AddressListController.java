7fab9d2366109eeab08d86631fa73dcad425594b
choijungwoo493 <jwoo1016@gmail.com>
2022-07-20 13:44:30 +0900

    [feat✨] : addressList Controller 생성

--- src/main/java/shop/gaship/gashipshoppingmall/adresslist/controller/AddressListController.java
+++ src/main/java/shop/gaship/gashipshoppingmall/adresslist/controller/AddressListController.java
@@ -1,8 +1,71 @@
 package shop.gaship.gashipshoppingmall.adresslist.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shop.gaship.gashipshoppingmall.adresslist.dto.AddressListPageResponseDto;
+import shop.gaship.gashipshoppingmall.adresslist.dto.AddressListRequestDto;
+import shop.gaship.gashipshoppingmall.adresslist.dto.AddressListResponseDto;
+import shop.gaship.gashipshoppingmall.adresslist.service.AddressListService;
+
 /**
  * @author 최정우
  * @since 1.0
  */
+@RestController
+@RequiredArgsConstructor
 public class AddressListController {
+    private final AddressListService addressListService;
+
+    @PostMapping("/members/{memberId}/addressLists")
+    public ResponseEntity<Void> AddressListAdd(@RequestBody AddressListRequestDto request) {
+        addressListService.addAddressList(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @PutMapping("/members/{memberId}/addressLists")
+    public ResponseEntity<Void> AddressListModify(@RequestBody AddressListRequestDto request) {
+        addressListService.modifyAddressList(request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @DeleteMapping("/members/{memberId}/addressLists/{addressListId}")
+    public ResponseEntity<Void> AddressListRemove(@PathVariable Integer addressListId) {
+        addressListService.deleteAddressList(addressListId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @GetMapping("/members/{memberId}/addressLists/{addressListId}")
+    public ResponseEntity<AddressListResponseDto> AddressListDetails(@PathVariable Integer addressListId) {
+        AddressListResponseDto addressListResponseDto = addressListService.findAddressList(addressListId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(addressListResponseDto);
+    }
+
+    @GetMapping("/members/{memberId}/addressLists")
+    public ResponseEntity<AddressListPageResponseDto> AddressListList() {
+        AddressListPageResponseDto addressListPageResponseDto = addressListService.findAddressLists();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(addressListPageResponseDto);
+    }
 }
---
+++ src/main/java/shop/gaship/gashipshoppingmall/adresslist/dto/AddressListRequestDto.java
@@ -0,0 +1,8 @@
+package shop.gaship.gashipshoppingmall.adresslist.dto;
+
+/**
+ * @author 최정우
+ * @since 1.0
+ */
+public class AddressListRequestDto {
+}
