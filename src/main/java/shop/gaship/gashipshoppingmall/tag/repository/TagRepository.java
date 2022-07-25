0c4c345958eedce67ccc39e96c8db56b51007885
choijungwoo493 <jwoo1016@gmail.com>
2022-07-25 16:59:52 +0900

    [refactor🔨] : 띄어쓰기 리팩토링

--- src/main/java/shop/gaship/gashipshoppingmall/tag/repository/TagRepository.java
+++ src/main/java/shop/gaship/gashipshoppingmall/tag/repository/TagRepository.java
@@ -18,5 +18,4 @@ public interface TagRepository extends JpaRepository<Tag, Integer>, TagRepositor
      * @return title 매개변수와 이미 있는 태그들의 태그명이 일치하는 경우가 있으면 false 아니면 true 를 반환합니다.
      */
     boolean existsByTitle(String title);
-
 }
