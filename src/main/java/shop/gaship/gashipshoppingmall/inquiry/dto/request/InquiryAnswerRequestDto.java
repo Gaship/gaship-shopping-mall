0c2bd23bd10120f5159dc605f38863c62c63d89a
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-07-30 11:52:42 +0900

    [refactor🔨] : dto 변경

--- src/main/java/shop/gaship/gashipshoppingmall/inquiry/dto/request/InquiryAnswerRequestDto.java
+++ src/main/java/shop/gaship/gashipshoppingmall/inquiry/dto/request/InquiryAnswerRequestDto.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.Setter;
 
 /**
  * 문의에 대한 답변을 등록하기위해 사용하는 DTO 클래스입니다.
@@ -12,16 +11,15 @@ import lombok.Setter;
  * @author : 최겸준
  * @since 1.0
  */
-@Setter
 @Getter
 public class InquiryAnswerRequestDto {
 
     @Min(1)
-    @NotNull(message = "inquriyNo 는 필수값입니다.")
+    @NotNull(message = "inquiryNo 는 필수값입니다.")
     private Integer inquiryNo;
 
-    @Min(0)
-    @NotNull(message = "employeeNo 는 필수값입니다.")
+    @Min(1)
+    @NotNull
     private Integer employeeNo;
 
     @NotBlank(message = "answerContent 는 필수값입니다.")
