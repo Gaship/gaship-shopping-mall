6f14bd67a655d3bf9973fced5722dbd5272059b1
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-07-30 11:49:34 +0900

    [refactor🔨] : private -> public으로 변경(StatusCode), 존재하지 않던 상수를 상수화(Employee)

--- src/main/java/shop/gaship/gashipshoppingmall/statuscode/exception/StatusCodeNotFoundException.java
+++ src/main/java/shop/gaship/gashipshoppingmall/statuscode/exception/StatusCodeNotFoundException.java
@@ -7,7 +7,7 @@ package shop.gaship.gashipshoppingmall.statuscode.exception;
  * @since 1.0
  */
 public class StatusCodeNotFoundException extends RuntimeException {
-    private static final String MESSAGE = "해당 상태 코드를 찾을 수 없습니다.";
+    public static final String MESSAGE = "해당 상태 코드를 찾을 수 없습니다.";
 
     public StatusCodeNotFoundException() {
         super(MESSAGE);
