8d55ecd245fe6bdef395ef8fda36bd2418dffa9d
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-08-02 13:09:26 +0900

    [feat✨] : 다건조회 요청과 반환에 대한 dto 생성

--- src/main/java/shop/gaship/gashipshoppingmall/inquiry/dto/request/InquirySearchRequestDto.java
+++ src/main/java/shop/gaship/gashipshoppingmall/inquiry/dto/request/InquirySearchRequestDto.java
@@ -1,7 +1,20 @@
-package shop.gaship.gashipshoppingmall.inquiry.dto.request;/**
- * 
- *  
- * @author : 최겸준 
+package shop.gaship.gashipshoppingmall.inquiry.dto.request;
+
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+/**
+ * 문의를 검색할때 넘어오는 파라미터들을 담기위한 dto 객체입니다.
+ *
+ * @author : 최겸준
  * @since 1.0
- */public class InquirySearchRequestDto {
+ */
+@Getter
+public class InquirySearchRequestDto {
+
+    @NotNull
+    private Boolean isProduct;
+    private String status;
+    private Integer memberNo;
+    private Integer productNo;
 }
--- src/main/java/shop/gaship/gashipshoppingmall/inquiry/dto/response/InquiryResponseDto.java
+++ src/main/java/shop/gaship/gashipshoppingmall/inquiry/dto/response/InquiryResponseDto.java
@@ -1,7 +1,27 @@
-package shop.gaship.gashipshoppingmall.inquiry.dto.response;/**
- * 
- *  
- * @author : 최겸준 
+package shop.gaship.gashipshoppingmall.inquiry.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+/**
+ * 조회시 필요한 정보만 프로젝션하기위한 dto이며 해당 dto가 클라이언트에게 반환됩니다.
+ *
+ * @author : 최겸준
  * @since 1.0
- */public class InquiryResponseDto {
+ */
+@Getter
+public class InquiryResponseDto {
+
+    private Integer inquiryNo;
+    private String memberNickname;
+    private String employeeName;
+    private String processStatus;
+    private String productName;
+
+    private String title;
+    private String inquiryContent;
+    private LocalDateTime registerDatetime;
+    private String answerContent;
+    private LocalDateTime answerRegisterDatetime;
+    private LocalDateTime answerModifyDatetime;
 }
