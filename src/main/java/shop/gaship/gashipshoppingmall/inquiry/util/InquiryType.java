bc9f86398e92da64c67a44bef07010f445d1e2cd
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-08-04 11:16:59 +0900

    [feat✨] : 문의 종류 구분하기위한 enum 추가

---
+++ src/main/java/shop/gaship/gashipshoppingmall/inquiry/util/InquiryType.java
@@ -0,0 +1,20 @@
+package shop.gaship.gashipshoppingmall.inquiry.util;
+
+import lombok.Getter;
+
+/**
+ * 컨트롤러나 서비스계층에서 가독성을 높이기 위해서 사용하는 enum 클래스입니다.
+ *
+ * @author 최겸준
+ * @since 1.0
+ */
+@Getter
+public enum InquiryType {
+    PRODUCT_INQUIRIES(Boolean.TRUE), CUSTOMER_INQUIRIES(Boolean.FALSE);
+
+    private final Boolean value;
+
+    InquiryType(Boolean value) {
+        this.value = value;
+    }
+}
