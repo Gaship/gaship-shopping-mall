8108352351176fff30660988d9ba4d97cadbb372
choijungwoo493 <jwoo1016@gmail.com>
2022-08-05 00:45:21 +0900

    [refactor🔨] : 페이징타입 변경(PageResponseDto->PageResponse)

--- src/main/java/shop/gaship/gashipshoppingmall/tag/repository/TagRepositoryCustom.java
+++ src/main/java/shop/gaship/gashipshoppingmall/tag/repository/TagRepositoryCustom.java
@@ -3,7 +3,7 @@ package shop.gaship.gashipshoppingmall.tag.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.NoRepositoryBean;
-import shop.gaship.gashipshoppingmall.tag.entity.Tag;
+import shop.gaship.gashipshoppingmall.tag.dto.response.TagResponseDto;
 
 /**
  * 태그관련 커스텀 쿼리를 위한 인터페이스입니다.
@@ -17,7 +17,7 @@ public interface TagRepositoryCustom {
      * 태그를 페이징하는 메서드입니다.
      *
      * @param pageable 조회하려는 태그 페이지 정보
-     * @return TagResponseDto의 페이징 객체가 반환됩니다.
+     * @return TagResponseDto 의 페이징 객체가 반환됩니다.
      */
-    Page<Tag> getAllTags(Pageable pageable);
+    Page<TagResponseDto> getTags(Pageable pageable);
 }
