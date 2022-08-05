1d1c5c866a7cac1469b0697a7b460583d5dfc803
oct-sky-out <kms3335k@naver.com>
2022-08-05 13:54:45 +0900

    [test🧪] : 직원의 지역 기반으로 설치 배송 조회 테스트 DB로 전환

---
+++ src/test/java/shop/gaship/gashipshoppingmall/employee/repository/EmployeeInstallOrderRepositoryTest.java
@@ -0,0 +1,45 @@
+package shop.gaship.gashipshoppingmall.employee.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import shop.gaship.gashipshoppingmall.config.DataProtectionConfig;
+import shop.gaship.gashipshoppingmall.config.DataSourceConfig;
+import shop.gaship.gashipshoppingmall.order.entity.Order;
+import shop.gaship.gashipshoppingmall.response.PageResponse;
+
+/**
+ * 설명작성란
+ *
+ * @author 김민수
+ * @since 1.0
+ */
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({DataProtectionConfig.class, DataSourceConfig.class})
+class EmployeeInstallOrderRepositoryTest {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Test
+    @DisplayName("직원이 자신의 지역에서 설치해야 할 주문들을 조회합니다.")
+    void findOrderBasedOnMyLocationTest() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        PageResponse<Order> result = employeeRepository.findOrderBasedOnEmployeeLocation(pageable, 1);
+
+        assertThat(result.getSize()).isEqualTo(10);
+        assertThat(result.getPage()).isZero();
+        assertThat(result.isNext()).isTrue();
+        assertThat(result.isPrev()).isFalse();
+        assertThat(result.getContent()).isNotEmpty();
+        System.out.println(result.getContent());
+    }
+}
