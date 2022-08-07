aba5f493f285d4202b62273f353f77adfdb59dc2
HoFe-U <hochul-y@naver.com>
2022-08-07 16:31:47 +0900

    [fix🚑] : totalsale 충돌, 관련 브랜치 새로 생성후 머지

---
+++ src/main/java/shop/gaship/gashipshoppingmall/totalsale/dto/request/TotalSaleRequestDto.java
@@ -0,0 +1,26 @@
+package shop.gaship.gashipshoppingmall.totalsale.dto.request;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * 일자를 통해 총 매출을 입력받기위한 클래스입니다.
+ *
+ * @author : 유호철
+ * @since 1.0
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TotalSaleRequestDto {
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endDate;
+
+}
---
+++ src/main/java/shop/gaship/gashipshoppingmall/totalsale/service/TotalSaleService.java
@@ -0,0 +1,23 @@
+package shop.gaship.gashipshoppingmall.totalsale.service;
+
+import java.util.List;
+import shop.gaship.gashipshoppingmall.totalsale.dto.request.TotalSaleRequestDto;
+import shop.gaship.gashipshoppingmall.totalsale.dto.response.TotalSaleResponseDto;
+
+/**
+ * 매출현황을 보기위한 interface 입니다.
+ *
+ * @author : 유호철
+ * @since 1.0
+ */
+public interface TotalSaleService {
+
+
+    /**
+     * 검색할 시작날과 끝날짜를 입력하면 관련 매출현황이 나오는 메서드입니다.
+     *
+     * @param dto 시작날짜와 끝날짜가 기입되어있습니다.
+     * @return 매출현황이 반환됩니다.
+     */
+    List<TotalSaleResponseDto> findTotalSales(TotalSaleRequestDto dto);
+}
