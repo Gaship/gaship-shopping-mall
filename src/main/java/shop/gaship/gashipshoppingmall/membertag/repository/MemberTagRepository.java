5877202c36f7ab4badd32a30d5f551729b8fcbed
choijungwoo493 <jwoo1016@gmail.com>
2022-08-08 21:26:37 +0900

    [refactor🔨] : 반환값 변경에 따른 테스트 수정

--- src/main/java/shop/gaship/gashipshoppingmall/membertag/repository/MemberTagRepository.java
+++ src/main/java/shop/gaship/gashipshoppingmall/membertag/repository/MemberTagRepository.java
@@ -14,15 +14,16 @@ public interface MemberTagRepository extends JpaRepository<MemberTag, Integer> {
     /**
      * 회원이 기존에 설정한 모든 태그를 지우고 등록하길 원하는 모든 태그를 등록하는 메서드입니다.
      *
-     * @param memberNo the member no
+     * @param memberNo 회원 id
      */
     void deleteAllByMember_MemberNo(Integer memberNo);
 
     /**
      * 회원이 기존에 설정한 모든 태그값을 DB에 접근해 가져오는 메서드입니다.
      *
-     * @param memberNo the member no
-     * @return the list
+     * @param memberNo 회원 id
+     * @return 회원이 등록한 회원 태그 리스트
      */
     List<MemberTag> findAllByMember_MemberNo(Integer memberNo);
+
 }
