e7dcb36487b86dccf24266cabbd8bc48fbbd18ff
semi <semikimm.m@gmail.com>
2022-08-09 14:28:21 +0900

    [feat✨] : object storage 계정 관련 config 및 properties 추가

---
+++ src/main/java/shop/gaship/gashipshoppingmall/config/ObjectStorageConfig.java
@@ -0,0 +1,46 @@
+package shop.gaship.gashipshoppingmall.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * object storage 관련 설정을 위한 configuration 입니다.
+ *
+ * @author : 김세미
+ * @since 1.0
+ */
+@Configuration
+@ConfigurationProperties(prefix = "objectstorage")
+@RequiredArgsConstructor
+public class ObjectStorageConfig {
+    private final DataProtectionConfig dataProtectionConfig;
+    private String tenantId;
+    private String password;
+    private String account;
+
+
+    public String getTenantId() {
+        return dataProtectionConfig.findSecretDataFromSecureKeyManager(tenantId);
+    }
+
+    public String getPassword() {
+        return dataProtectionConfig.findSecretDataFromSecureKeyManager(password);
+    }
+
+    public String getAccount() {
+        return dataProtectionConfig.findSecretDataFromSecureKeyManager(account);
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
