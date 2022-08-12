0dc871a3459b25c83b65972bb0551a8f22577a03
조재철 <rhsnqk@gmail.com>
2022-08-12 19:12:02 +0900

    [test🧪] : controller 테스트 추가

--- src/test/java/shop/gaship/gashipshoppingmall/delivery/controller/DeliveryControllerTest.java
+++ src/test/java/shop/gaship/gashipshoppingmall/delivery/controller/DeliveryControllerTest.java
@@ -1,20 +1,67 @@
 package shop.gaship.gashipshoppingmall.delivery.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import shop.gaship.gashipshoppingmall.delivery.dto.response.TrackingNoResponseDto;
+import shop.gaship.gashipshoppingmall.delivery.service.impl.DeliveryServiceImpl;
 
 /**
  * @author : 조재철
  * @since 1.0
  */
+@WebMvcTest(DeliveryController.class)
 class DeliveryControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    DeliveryServiceImpl deliveryService;
+
     @Test
-    void createTrackingNo() {
+    void createTrackingNo() throws Exception {
+
+        // given
+        doNothing().when(deliveryService).createTrackingNo(1);
+
+        // when then
+        mockMvc.perform(
+                   get("/api/delivery/tracking-no/" + 1))
+               .andExpect(status().isOk());
+
+        verify(deliveryService).createTrackingNo(1);
     }
 
     @Test
-    void addTrackingNo() {
+    void addTrackingNo() throws Exception {
+
+        // given
+        doNothing().when(deliveryService).addTrackingNo(any());
+
+        TrackingNoResponseDto trackingNoResponseDto = new TrackingNoResponseDto("1", "1");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(trackingNoResponseDto);
+
+        // when then
+        mockMvc.perform(
+                   post("/eggplant/tracking-no")
+                       .contentType(MediaType.APPLICATION_JSON)
+                       .content(content))
+               .andExpect(status().isOk());
+
+        verify(deliveryService).addTrackingNo(any());
     }
 }
