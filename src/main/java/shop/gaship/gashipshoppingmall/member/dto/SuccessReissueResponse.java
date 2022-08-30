418e447f23c44513c621ad834a3af5a9771c7665
oct-sky-out <kms3335k@naver.com>
2022-08-31 04:02:02 +0900

    [fix🚑] : 비밀번호 재발급 에러 해결

--- src/main/java/shop/gaship/gashipshoppingmall/member/dto/SuccessReissueResponse.java
+++ src/main/java/shop/gaship/gashipshoppingmall/member/dto/SuccessReissueResponse.java
@@ -2,6 +2,7 @@ package shop.gaship.gashipshoppingmall.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 비밀번호 재발급 요청에 대한 응답객체입니다.
@@ -11,6 +12,7 @@ import lombok.Getter;
  */
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SuccessReissueResponse {
     private String email;
     private String reissuedPassword;
