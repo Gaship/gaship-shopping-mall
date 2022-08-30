0ec21a1cc4ccc85d3713be5809162971b90418bf
HoFe-U <hochul-y@naver.com>
2022-08-30 22:31:55 +0900

    [test🧪] : 상품상세조회 테스트 코드추가

--- src/test/java/shop/gaship/gashipshoppingmall/orderproduct/controller/OrderProductControllerTest.java
+++ src/test/java/shop/gaship/gashipshoppingmall/orderproduct/controller/OrderProductControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import shop.gaship.gashipshoppingmall.orderproduct.dto.response.OrderProductDetailResponseDto;
 import shop.gaship.gashipshoppingmall.orderproduct.dto.response.OrderProductResponseDto;
 import shop.gaship.gashipshoppingmall.orderproduct.service.OrderProductService;
 
@@ -69,4 +70,35 @@ class OrderProductControllerTest {
             .andExpect(status().isOk())
             .andDo(print());
     }
+
+    @DisplayName("상품번호에 해당하는 상품 상세표시")
+    @Test
+    void orderProductDetail() throws Exception {
+        OrderProductDetailResponseDto dto =
+            new OrderProductDetailResponseDto(1, 1, "product", 1L, "status", "uuid", "color", "manufacturer"
+                , "korea", "seller", "importer", "qq", "explain");
+        dto.setFilePath("file");
+        when(orderProductService.findMemberOrderProductDetail(anyInt()))
+            .thenReturn(dto);
+
+        mvc.perform(get("/api/order-products/{orderProductNo}", 1)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.productNo").value(objectMapper.writeValueAsString(dto.getProductNo())))
+            .andExpect(jsonPath("$.orderNo").value(objectMapper.writeValueAsString(dto.getOrderNo())))
+            .andExpect(jsonPath("$.productName").value((dto.getProductName())))
+            .andExpect(jsonPath("$.totalOrderAmount").value((dto.getTotalOrderAmount())))
+            .andExpect(jsonPath("$.orderProductStatus").value((dto.getOrderProductStatus())))
+            .andExpect(jsonPath("$.trackingNo").value((dto.getTrackingNo())))
+            .andExpect(jsonPath("$.color").value((dto.getColor())))
+            .andExpect(jsonPath("$.manufacturer").value((dto.getManufacturer())))
+            .andExpect(jsonPath("$.manufacturerCountry").value((dto.getManufacturerCountry())))
+            .andExpect(jsonPath("$.seller").value((dto.getSeller())))
+            .andExpect(jsonPath("$.importer").value((dto.getImporter())))
+            .andExpect(jsonPath("$.qualityAssuranceStandard").value((dto.getQualityAssuranceStandard())))
+            .andExpect(jsonPath("$.explanation").value((dto.getExplanation())))
+            .andExpect(jsonPath("$.filePath").value(("file")))
+            .andDo(print());
+    }
+
 }
