da911fbfe5101bbf329ea121c4af801bddbce992
HoFe-U <hochul-y@naver.com>
2022-09-01 13:27:45 +0900

    [test🧪] : 주문조회 수정으로인한 test 수정

--- src/test/java/shop/gaship/gashipshoppingmall/orderproduct/controller/OrderProductControllerTest.java
+++ src/test/java/shop/gaship/gashipshoppingmall/orderproduct/controller/OrderProductControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import shop.gaship.gashipshoppingmall.orderproduct.dto.response.OrderProductDetailResponseDto;
@@ -45,8 +46,8 @@ class OrderProductControllerTest {
     @Test
     void orderproductFindByMember() throws Exception {
         List<OrderProductResponseDto> list = new ArrayList<>();
-        OrderProductResponseDto dto = new OrderProductResponseDto(1, 1, "test", 100L, LocalDateTime.now(), "유유", "010", "배송",
-            "uuid");
+        OrderProductResponseDto dto = new OrderProductResponseDto(1, 100L, LocalDateTime.now(), "유유",
+            "receipt");
         PageRequest pageRequest = PageRequest.of(0, 10);
         list.add(dto);
         PageImpl<OrderProductResponseDto> page = new PageImpl<>(list, pageRequest, 1);
@@ -58,14 +59,10 @@ class OrderProductControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .queryParam("page", objectMapper.writeValueAsString(pageRequest.getPageNumber()))
                 .queryParam("size", objectMapper.writeValueAsString(pageRequest.getPageSize())))
-            .andExpect(jsonPath("$.content.[0].orderProductNo").value(dto.getOrderProductNo()))
             .andExpect(jsonPath("$.content.[0].orderNo").value(dto.getOrderNo()))
-            .andExpect(jsonPath("$.content.[0].productName").value(dto.getProductName()))
             .andExpect(jsonPath("$.content.[0].totalOrderAmount").value(dto.getTotalOrderAmount()))
             .andExpect(jsonPath("$.content.[0].receiptName").value(dto.getReceiptName()))
             .andExpect(jsonPath("$.content.[0].receiptPhoneNumber").value(dto.getReceiptPhoneNumber()))
-            .andExpect(jsonPath("$.content.[0].orderStatus").value(dto.getOrderStatus()))
-            .andExpect(jsonPath("$.content.[0].trackingNo").value(dto.getTrackingNo()))
 
             .andExpect(status().isOk())
             .andDo(print());
@@ -74,32 +71,37 @@ class OrderProductControllerTest {
     @DisplayName("상품번호에 해당하는 상품 상세표시")
     @Test
     void orderProductDetail() throws Exception {
+        PageRequest pageRequest = PageRequest.of(0, 10);
         OrderProductDetailResponseDto dto =
             new OrderProductDetailResponseDto(1, 1, "product", 1L, "status", "uuid", "color", "manufacturer"
                 , "korea", "seller", "importer", "qq", "explain", 1, "address", "zipCode", "name", "010", "0101", LocalDateTime.now(), 10L, "");
         dto.setFilePath("file");
-        when(orderProductService.findMemberOrderProductDetail(anyInt(), anyInt()))
-            .thenReturn(dto);
+        List<OrderProductDetailResponseDto> list = List.of(dto);
+        PageImpl<OrderProductDetailResponseDto> page = new PageImpl<>(list);
+        when(orderProductService.findMemberOrderProductDetail(anyInt(), anyInt(), any(Pageable.class)))
+            .thenReturn(page);
 
         mvc.perform(get("/api/order-products/{orderProductNo}", 1)
                 .contentType(MediaType.APPLICATION_JSON)
-                .queryParam("memberNo", objectMapper.writeValueAsString(1)))
+                .queryParam("memberNo", objectMapper.writeValueAsString(1))
+                .queryParam("page", objectMapper.writeValueAsString(pageRequest.getPageNumber()))
+                .queryParam("size", objectMapper.writeValueAsString(pageRequest.getPageSize())))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.productNo").value(objectMapper.writeValueAsString(dto.getProductNo())))
-            .andExpect(jsonPath("$.orderNo").value(objectMapper.writeValueAsString(dto.getOrderNo())))
-            .andExpect(jsonPath("$.productName").value((dto.getProductName())))
-            .andExpect(jsonPath("$.totalOrderAmount").value((dto.getTotalOrderAmount())))
-            .andExpect(jsonPath("$.orderProductStatus").value((dto.getOrderProductStatus())))
-            .andExpect(jsonPath("$.trackingNo").value((dto.getTrackingNo())))
-            .andExpect(jsonPath("$.color").value((dto.getColor())))
-            .andExpect(jsonPath("$.manufacturer").value((dto.getManufacturer())))
-            .andExpect(jsonPath("$.manufacturerCountry").value((dto.getManufacturerCountry())))
-            .andExpect(jsonPath("$.seller").value((dto.getSeller())))
-            .andExpect(jsonPath("$.importer").value((dto.getImporter())))
-            .andExpect(jsonPath("$.qualityAssuranceStandard").value((dto.getQualityAssuranceStandard())))
-            .andExpect(jsonPath("$.explanation").value((dto.getExplanation())))
-            .andExpect(jsonPath("$.filePath").value(("file")))
-            .andExpect(jsonPath("$.memberNo").value(dto.getMemberNo()))
+            .andExpect(jsonPath("$.content[0].productNo").value(objectMapper.writeValueAsString(dto.getProductNo())))
+            .andExpect(jsonPath("$.content[0].orderNo").value(objectMapper.writeValueAsString(dto.getOrderNo())))
+            .andExpect(jsonPath("$.content[0].productName").value((dto.getProductName())))
+            .andExpect(jsonPath("$.content[0].totalOrderAmount").value((dto.getTotalOrderAmount())))
+            .andExpect(jsonPath("$.content[0].orderProductStatus").value((dto.getOrderProductStatus())))
+            .andExpect(jsonPath("$.content[0].trackingNo").value((dto.getTrackingNo())))
+            .andExpect(jsonPath("$.content[0].color").value((dto.getColor())))
+            .andExpect(jsonPath("$.content[0].manufacturer").value((dto.getManufacturer())))
+            .andExpect(jsonPath("$.content[0].manufacturerCountry").value((dto.getManufacturerCountry())))
+            .andExpect(jsonPath("$.content[0].seller").value((dto.getSeller())))
+            .andExpect(jsonPath("$.content[0].importer").value((dto.getImporter())))
+            .andExpect(jsonPath("$.content[0].qualityAssuranceStandard").value((dto.getQualityAssuranceStandard())))
+            .andExpect(jsonPath("$.content[0].explanation").value((dto.getExplanation())))
+            .andExpect(jsonPath("$.content[0].filePath").value(("file")))
+            .andExpect(jsonPath("$.content[0].memberNo").value(dto.getMemberNo()))
             .andDo(print());
     }
 
